b7268cc378ba7dedd407f0936f6b7153c7aee4c3
Shahar Rose <rose.shahar@gmail.com>
2024-03-10 21:06:40 +0200

    convert everything to new react state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Line } from "react-chartjs-2";
-import { Chart as ChartJS, CategoryScale } from "chart.js/auto";
+import { CategoryScale, Chart as ChartJS } from "chart.js/auto";
 import moment from "moment";
 import Button from "react-bootstrap/Button";
 
@@ -48,44 +48,37 @@ export const options = {
   }
 };
 
-class App extends React.Component {
-  state = {
-    data_ready: false,
-    etf_data: {},
-    graph_data: {},
-    labels: [],
-    historic_month_count: 1
-  };
-  chart = React.createRef();
+function App() {
+  const [dataReady, setDataReady] = useState(false);
+  const [etfData, setEtfData] = useState({});
+  const [graphData, setGraphData] = useState({});
+  const [historicMonthCount, setHistoricMonthCount] = useState(1);
 
-  async componentDidMount() {
-    await this.requestData();
-  }
 
-  async fetch_etf(etf) {
+  async function fetch_etf(etf) {
     const today = moment(Date.now()).format("YYYY-MM-DD");
 
     let start_date = new Date();
-    start_date.setMonth(start_date.getMonth() - this.state.historic_month_count);
+    start_date.setMonth(start_date.getMonth() - historicMonthCount);
     start_date = moment(start_date).format("YYYY-MM-DD");
-    console.log("start_date", start_date, this.state.historic_month_count);
+    console.log("start_date", start_date, historicMonthCount);
 
     const response = await fetch(`https://www.funder.co.il/wsStock.asmx/GetEtfTickerm?callback=&id=${etf}&startDate=${start_date}&endDate=${today}`);
     const etf_data = await response.json();
 
-    this.state.etf_data[etf] = etf_data["x"];
+    etfData[etf] = etf_data["x"];
   }
 
-  requestData = async () => {
+  let requestData = async () => {
     let etfs = [1159094, 1159250, 1146646];
-    await Promise.all(etfs.map(etf => this.fetch_etf(etf)));
+    await Promise.all(etfs.map(etf => fetch_etf(etf)));
 
 
     let data_sets = [];
     let labels = [];
 
-    for (let key in this.state.etf_data) {
-      let curr_data = this.state.etf_data[key];
+    for (let key in etfData) {
+      let curr_data = etfData[key];
       let base = curr_data[0].p;
 
       data_sets.push(
@@ -98,54 +91,55 @@ class App extends React.Component {
       labels = curr_data.map(item => item.c);
     }
 
-    console.log(this.state.etf_data);
+    console.log(etfData);
     console.log(labels);
 
-    this.setState(
-      {
-        graph_data: {
-          labels: labels,
-          datasets: data_sets
-        },
-        data_ready: true
-      }
-    );
+    setGraphData({
+      labels: labels,
+      datasets: data_sets
+    });
+    setDataReady(true);
   };
 
-  render() {
-    if (!this.state.data_ready) {
-      return <div>Loading...</div>;
-    }
+  useEffect(() => {
+    requestData().then();
+  }, []);
 
-    return (
-      <div className="App">
-        <h1>hello</h1>
-        <div>
-          <Button variant="secondary"
-                  onClick={() => {
-                    this.setState({ historic_month_count: 1, data_ready: false });
-                    this.requestData().then(r => {
-                    });
-                  }}>1 Month</Button>
-          <Button variant="secondary"
-                  onClick={() => {
-                    this.setState({ historic_month_count: 3, data_ready: false });
-                    this.requestData().then(r => {
-                    });
-                  }}>3 Months</Button>
-          <Button variant="secondary"
-                  onClick={() => {
-                    this.setState({ historic_month_count: 6, data_ready: false });
-                    this.requestData().then(r => {
-                    });
-                  }}>6 Months</Button>
-        </div>
-        <header className="App-header">
-          <Line ref={this.chart} options={options} data={this.state.graph_data} redraw={true} />;
-        </header>
-      </div>
-    );
+  if (!dataReady) {
+    return <div>Loading...</div>;
   }
+
+  return (
+    <div className="App">
+      <h1>hello</h1>
+      <div>
+        <Button variant="secondary"
+                onClick={() => {
+                  setDataReady(false);
+                  setHistoricMonthCount(1);
+                  requestData().then(r => {
+                  });
+                }}>1 Month</Button>
+        <Button variant="secondary"
+                onClick={() => {
+                  setDataReady(false);
+                  setHistoricMonthCount(2);
+                  requestData().then(r => {
+                  });
+                }}>3 Months</Button>
+        <Button variant="secondary"
+                onClick={() => {
+                  setDataReady(false);
+                  setHistoricMonthCount(3);
+                  requestData().then(r => {
+                  });
+                }}>6 Months</Button>
+      </div>
+      <header className="App-header">
+        <Line options={options} data={graphData} redraw={true} />;
+      </header>
+    </div>
+  );
 }
 
 export default App;
